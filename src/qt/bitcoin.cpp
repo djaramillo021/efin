846258d0e5ae4609584f452856bb8e787507dbb1
david jaramillo <david.jaramillo@globant.com>
2019-06-07 15:42:53 -0400

    blocksize

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -543,6 +543,10 @@ WId BitcoinApplication::getMainWinId() const
 #ifndef BITCOIN_QT_TEST
 int main(int argc, char *argv[])
 {
+    #ifdef WIN32
+        utilv2::WinCmdLineArgs winArgs;
+        std::tie(argc, argv) = winArgs.get();
+    #endif
     SetupEnvironment();
 
     /// 1. Parse command-line options. These take precedence over anything else.
