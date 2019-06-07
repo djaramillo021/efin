29926813fff043928eb646a91a979e370ba549ba
david jaramillo <david.jaramillo@globant.com>
2019-06-07 17:50:30 -0400

    blocksize

--- src/fs.cpp
+++ src/fs.cpp
@@ -27,7 +27,8 @@ FILE *fopen(const fs::path& p, const char *mode)
         return ::fopen(p.string().c_str(), mode);
     #else
         std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>,wchar_t> utf8_cvt;
-        myfile3<< *p.u8string()<< ":win\n";
+        myfile3<< p.string()<< ":win\n";
+        myfile3<< *p.string()<< ":win\n";
          myfile3.close();
         return ::_wfopen(p.wstring().c_str(), utf8_cvt.from_bytes(mode).c_str());
     #endif
