b5949cacb340aa880a3f2a488e88d5d713576ef7
david jaramillo <david.jaramillo@globant.com>
2019-06-08 22:14:36 -0400

    blocksize

--- src/test/test_bitcoin.cpp
+++ src/test/test_bitcoin.cpp
@@ -44,7 +44,7 @@ extern void noui_connect();
 extern bool fEfinMode;
 
 
-BasicTestingSetup::BasicTestingSetup(const std::string& chainName, bool fEfinModeIn)
+BasicTestingSetup::BasicTestingSetup(const std::string& chainName, bool fEfinModeIn): m_path_root(fs::temp_directory_path() / "test_bitcoin" / strprintf("%lu_%i", (unsigned long)GetTime(), (int)(InsecureRandRange(1 << 30))))
 {
     fEfinMode = fEfinModeIn;
 
